88857d70d90b26c5778a2dfa142da47522d0faab
Kabir Doshi <kabirdoshi@gmail.com>
2021-03-21 22:26:55 -0400

    added upcoming events section

---
+++ src/MyCalendar.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import Calendar from "react_google_calendar"
+import './events.scss'
+
+const calendar_configuration = {
+    api_key: 'AIzaSyDFxU-YHy8b1p5quKxjaB_UTWW0Hu6PgNE',
+    calendars: [
+      {
+        name: 'demo',
+        url: 'gtbigdata@gmail.com'
+      }
+    ],
+    dailyRecurrence: 700,
+    weeklyRecurrence: 500,
+    monthlyRecurrence: 20
+}
+
+export default class BDBICalendar extends Component {
+    constructor(props) {
+      super(props)
+        this.state = {
+          events: []
+        }
+    }
+
+    render = () =>
+    <>
+        <h1 className = "events">Upcoming Events</h1>
+        <div className= 'calendar-container'>
+            <Calendar
+                events={this.state.events}
+                config={calendar_configuration}
+            />
+        </div>
+    </>
+        
+        
+}
+
+
