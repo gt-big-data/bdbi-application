c20373084d77a4963d9fc9cfe9a60c9302d5ff77
SanketManjesh <sanket.manjesh@gmail.com>
2021-03-26 06:02:08 -0400

    made contact page more responsive

--- src/Contact.js
+++ src/Contact.js
@@ -53,6 +53,10 @@ function Contact() {
                      <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class={cont.shapeFill}></path>
             </svg>
             </div>
+            <div className={cont.contentContain}>
+            <div className={cont.bottomLine}>
+
+            </div>
             <div className={cont.inputContain}>
                 <div className={cont.contactTitle}>
                     Contact Us
@@ -80,21 +84,27 @@ function Contact() {
                 </div>
                 <div className={cont.grid}>
                     <div className={cont.gridItem1}>
-                        <img src={insta}></img>
+                        <a href='https://www.instagram.com/'>
+                            <img src={insta}></img>
+                        </a>
                     </div>
                     <div className={cont.gridItem2}>
-                        <img src={link}></img>
+                        <a href = 'https://www.linkedin.com/home'>
+                            <img src={link}></img>
+                        </a>
                     </div>
                     <div className={cont.gridItem3}>
-                        <img src={fb}></img>
+                        <a href='https://www.facebook.com/'>
+                            <img src={fb}></img>
+                        </a>
                     </div>
                     <div className={cont.gridItem4}>
-                        <img src={slack}></img>
+                        <a href = 'https://slack.com'>
+                            <img src={slack}></img>
+                        </a>
                     </div>
                 </div>
             </div>
-            <div className={cont.bottomLine}>
-
             </div>
         </div>
     )
